5c182a6b7a7e68093194ae63dc106cdaae819c2d
enderdaniil <54796186+enderdaniil@users.noreply.github.com>
2019-11-02 19:19:46 +0300

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -15,9 +15,9 @@ int main()
 
 	int kolvo = 1;
 
-	for (int i = 0; i < a.length(); i++)
+	for (int i = 0; i < a.length()-1; i++)
 	{
-		if (a[i] == ' ')
+		if ((a[i] == ' ') && (a[i + 1] != ' '))
 		{
 			kolvo++;
 		}
