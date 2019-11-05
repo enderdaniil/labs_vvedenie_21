c4d27295e652b846d3e93073c3331372a710cd77
enderdaniil <54796186+enderdaniil@users.noreply.github.com>
2019-11-05 15:56:09 +0300

    Create negotovo4.cpp

---
+++ negotovo4.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+
+using namespace std;
+
+
+void top(char x)
+{
+	char arr[] = { 'а', 'ё', 'у', 'е', 'ы', 'о', 'э', 'я', 'и', 'ю' };
+	for (int i = 0; i )
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russia");
+
+	string a;
+	cout << "Vvedite Stroku" << endl;
+	getline(cin, a);
+
+	int kolvo = 0;
+
+	for (int i = 1; i < a.length(); i++)
+	{
+		
+	}
+
+	cout << "otvet = " << kolvo << endl;
+
+	return 0;
+
+}
